2f518ba1f85e1daa215296a98bd6e5204ca9f5cb
Honglin Chen <honglinc@stanford.edu>
2022-05-09 17:20:48 -0700

    add eisen-related scripts

---
+++ core/resnet.py
@@ -0,0 +1,214 @@
+"""
+Adapt from Detectron
+"""
+# Copyright (c) Facebook, Inc. and its affiliates.
+import fvcore.nn.weight_init as weight_init
+import torch.nn.functional as F
+
+from detectron2.layers import CNNBlockBase, Conv2d, get_norm, ShapeSpec
+from detectron2.modeling import BACKBONE_REGISTRY
+from detectron2.modeling.backbone.resnet import (
+    BasicStem,
+    BottleneckBlock,
+    DeformBottleneckBlock,
+    ResNet,
+)
+
+import torch.nn as nn
+import torch
+
+
+class DeepLabStem(CNNBlockBase):
+    """
+    The DeepLab ResNet stem (layers before the first residual block).
+    """
+
+    def __init__(self, in_channels=3, out_channels=128, max_pool=True, norm="BN"):
+        """
+        Args:
+            norm (str or callable): norm after the first conv layer.
+                See :func:`layers.get_norm` for supported format.
+        """
+        super().__init__(in_channels, out_channels, 4)
+        self.in_channels = in_channels
+        self.conv1 = Conv2d(
+            in_channels,
+            out_channels // 2,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            bias=False,
+            norm=get_norm(norm, out_channels // 2),
+        )
+        self.conv2 = Conv2d(
+            out_channels // 2,
+            out_channels // 2,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=False,
+            norm=get_norm(norm, out_channels // 2),
+        )
+        self.conv3 = Conv2d(
+            out_channels // 2,
+            out_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=False,
+            norm=get_norm(norm, out_channels),
+        )
+        weight_init.c2_msra_fill(self.conv1)
+        weight_init.c2_msra_fill(self.conv2)
+        weight_init.c2_msra_fill(self.conv3)
+        self.max_pool = max_pool
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu_(x)
+        x = self.conv2(x)
+        x = F.relu_(x)
+        x = self.conv3(x)
+        x = F.relu_(x)
+        if self.max_pool:
+            x = F.max_pool2d(x, kernel_size=3, stride=2, padding=1)
+        return x
+
+
+def build_resnet_deeplab_backbone(pool):
+    """
+    Create a ResNet instance from config.
+    Returns:
+        ResNet: a :class:`ResNet` instance.
+    """
+    # need registration of new blocks/stems?
+
+
+    # fmt: off
+    norm                = 'BN'                      # cfg.MODEL.RESNETS.NORM
+    stem_type           = 'deeplab'                     # cfg.MODEL.RESNETS.STEM_TYPE
+    freeze_at           = 0                             # cfg.MODEL.BACKBONE.FREEZE_AT
+    out_features        = ['res2', 'res3', 'res4']      # cfg.MODEL.RESNETS.OUT_FEATURES
+    depth               = 50                            # cfg.MODEL.RESNETS.DEPTH
+    num_groups          = 1                             # cfg.MODEL.RESNETS.NUM_GROUPS
+    width_per_group     = 64                            # cfg.MODEL.RESNETS.WIDTH_PER_GROUP
+    bottleneck_channels = num_groups * width_per_group
+    in_channels         = 128                           # cfg.MODEL.RESNETS.STEM_OUT_CHANNELS
+    out_channels        = 256                           # cfg.MODEL.RESNETS.RES2_OUT_CHANNELS
+    stride_in_1x1       = False                         # cfg.MODEL.RESNETS.STRIDE_IN_1X1
+    res4_dilation       = 1                             # cfg.MODEL.RESNETS.RES4_DILATION
+    res5_dilation       = 2                             # cfg.MODEL.RESNETS.RES5_DILATION
+    deform_on_per_stage = [False] * 4                   # cfg.MODEL.RESNETS.DEFORM_ON_PER_STAGE
+    deform_modulated    = False                         # cfg.MODEL.RESNETS.DEFORM_MODULATED
+    deform_num_groups   = 1                             # cfg.MODEL.RESNETS.DEFORM_NUM_GROUPS
+    res5_multi_grid     = [1, 2, 4]                     # cfg.MODEL.RESNETS.RES5_MULTI_GRID
+    # fmt: on
+
+    input_shape = ShapeSpec(channels=3)
+
+    if stem_type == "basic":
+        stem = BasicStem(
+            in_channels=input_shape.channels,
+            out_channels=in_channels,
+            norm=norm,
+        )
+    elif stem_type == "deeplab":
+        stem = DeepLabStem(
+            in_channels=input_shape.channels,
+            out_channels=in_channels,
+            norm=norm,
+            max_pool=pool
+        )
+    else:
+        raise ValueError("Unknown stem type: {}".format(stem_type))
+
+    assert res4_dilation in {1, 2}, "res4_dilation cannot be {}.".format(res4_dilation)
+    assert res5_dilation in {1, 2, 4}, "res5_dilation cannot be {}.".format(res5_dilation)
+    if res4_dilation == 2:
+        # Always dilate res5 if res4 is dilated.
+        assert res5_dilation == 4
+
+    num_blocks_per_stage = {50: [3, 4, 6, 3], 101: [3, 4, 23, 3], 152: [3, 8, 36, 3]}[depth]
+
+    stages = []
+
+    # Avoid creating variables without gradients
+    # It consumes extra memory and may cause allreduce to fail
+    out_stage_idx = [{"res2": 2, "res3": 3, "res4": 4, "res5": 5}[f] for f in out_features]
+    max_stage_idx = max(out_stage_idx)
+    for idx, stage_idx in enumerate(range(2, max_stage_idx + 1)):
+        if stage_idx == 4:
+            dilation = res4_dilation
+        elif stage_idx == 5:
+            dilation = res5_dilation
+        else:
+            dilation = 1
+        first_stride = 1 if idx == 0 or dilation > 1 else 2
+        stage_kargs = {
+            "num_blocks": num_blocks_per_stage[idx],
+            "stride_per_block": [first_stride] + [1] * (num_blocks_per_stage[idx] - 1),
+            "in_channels": in_channels,
+            "out_channels": out_channels,
+            "norm": norm,
+        }
+        stage_kargs["bottleneck_channels"] = bottleneck_channels
+        stage_kargs["stride_in_1x1"] = stride_in_1x1
+        stage_kargs["dilation"] = dilation
+        stage_kargs["num_groups"] = num_groups
+        if deform_on_per_stage[idx]:
+            stage_kargs["block_class"] = DeformBottleneckBlock
+            stage_kargs["deform_modulated"] = deform_modulated
+            stage_kargs["deform_num_groups"] = deform_num_groups
+        else:
+            stage_kargs["block_class"] = BottleneckBlock
+        if stage_idx == 5:
+            stage_kargs.pop("dilation")
+            stage_kargs["dilation_per_block"] = [dilation * mg for mg in res5_multi_grid]
+        blocks = ResNet.make_stage(**stage_kargs)
+        in_channels = out_channels
+        out_channels *= 2
+        bottleneck_channels *= 2
+        stages.append(blocks)
+    return ResNet(stem, stages, out_features=out_features).freeze(freeze_at)
+
+
+class ResNetFPN(nn.Module):
+    def __init__(self, min_level, max_level, channels={2: 256, 3: 512, 4: 1024}, latent_dim=512, pool=True):
+        super(ResNetFPN, self).__init__()
+
+        self.levels = list(range(min_level, max_level + 1))
+        self.min_level = min_level
+        self.max_level = max_level
+
+        # Backbone
+        self.backbone = build_resnet_deeplab_backbone(pool=pool)
+
+        # lateral conv for multi-level features concatenation
+        for level in self.levels:
+            setattr(self, 'lateral_conv_%d' % level, Conv2d(channels[level], latent_dim, kernel_size=1, bias=True))
+            nn.init.kaiming_normal_(getattr(self, 'lateral_conv_%d' % level).weight, mode='fan_out', nonlinearity='relu')
+
+        self.output_dim = latent_dim * len(self.levels)
+
+    def forward(self, x):
+        features = self.backbone(x)
+        output = self.concat_multi_level(features)  # construct feature pyramid instead
+
+        return output[self.min_level]
+
+    def concat_multi_level(self, features):
+
+        feats_lateral = {l: getattr(self, 'lateral_conv_%d' % l)(features['res%d' % l]) for l in self.levels}
+
+        # Adds top-down path.
+        backbone_max_level = self.levels[-1]
+        feats = {backbone_max_level: feats_lateral[backbone_max_level]}
+
+        for level in range(backbone_max_level - 1, self.min_level - 1, -1):
+            feats[level] = torch.cat([
+                F.interpolate(feats[level + 1], scale_factor=2.0, mode='nearest'),
+                feats_lateral[level]
+            ], 1)
+
+        return feats
+
---
+++ core/teacher_student.py
@@ -0,0 +1,238 @@
+from functools import partial
+from pathlib import Path
+import argparse
+import copy
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+from core.teachers import MotionToStaticTeacher
+from core.eisen import EISEN
+from core.utils.segmentation_metrics import measure_static_segmentation_metric
+from teachers import get_args
+import matplotlib.pyplot as plt
+import os
+
+def _get_model_class(name):
+    cls = None
+    if 'motion_to_static' in name:
+        cls = MotionToStaticTeacher
+    elif 'eisen' in name:
+        cls = EISEN
+    else:
+        raise ValueError("Couldn't identify a model class associated with %s" % name)
+    return cls
+
+
+def load_model(model_class, load_path, freeze, **kwargs):
+    if model_class == 'raft_pretrained':
+        return None # load from saved flows from pretrained models
+    cls = _get_model_class(model_class)
+    assert cls is not None, "Wasn't able to infer model class"
+
+    # build model
+    model = cls(**kwargs)
+
+    # load checkpoint
+    if load_path is not None:
+        weight_dict = torch.load(load_path)
+        new_dict = dict()
+        for k in weight_dict.keys():
+            if 'module' in k:
+                new_dict[k.split('module.')[-1]] = weight_dict[k]
+        did_load = model.load_state_dict(new_dict, strict=False)
+        print(did_load, type(model).__name__, load_path)
+
+    # freeze params if needed
+    if freeze:
+        for param in model.parameters():
+            param.requires_grad = False
+
+    return model
+
+
+class TeacherStudent(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        assert args.teacher_class in ['raft_pretrained', 'motion_to_static', 'motion_to_static_v1'], f"Unexpected teacher class {args.teacher_class}"
+        print('Teacher class: ', args.teacher_class)
+        self.teacher = load_model(args.teacher_class, args.teacher_load_path, freeze=True, **args.teacher_params)
+        self.student = load_model(args.student_class, args.student_load_path, freeze=False, **args.student_params)
+        self.args = args
+
+    def forward(self, img1, img2, gt_segment, iters, raft_moving=None, get_segments=False):
+
+        if self.args.teacher_class == 'raft_pretrained':
+            target = raft_moving
+        elif self.args.teacher_class == 'motion_to_static_v1':
+            motion_thresh = boundary_thresh = 0.5
+            self.teacher.target_model.motion_thresh = motion_thresh
+            self.teacher.target_model.boundary_thresh = boundary_thresh
+            target = self.teacher(img1, img2, flow_iters=24, bootstrap=True)['target'] + 1
+
+            print('target.size: ', target.shape)
+        else:
+            self.teacher.eval()
+            with torch.no_grad():
+                target = self.teacher(img1, img2) + 1 # add 1 so that the background has value zero
+
+        # self.visualize_targets(img1, target)
+        assert target.min() >= 0, target.min()
+        affinity, loss, pred_segment = self.student(img1, target, get_segments=get_segments)
+
+        metric = {'loss': loss.item()}
+        if get_segments:
+            seg_metric, visuals = self.measure_segments(pred_segment, gt_segment)
+            # self.visualize_segments(visuals, target, img1)
+            self.visualize_segments_all(pred_segment, gt_segment, target, img1 / 255.)
+            metric['miou'] = seg_metric['metric_pred_segment_mean_ious'].item()
+
+        return loss, metric
+
+    def measure_segments(self, pred_segment, gt_segment):
+
+       return measure_static_segmentation_metric({'pred_segment': pred_segment}, {'gt_segment': gt_segment}, pred_segment.shape[-2:],
+                                           segment_key=['pred_segment'],
+                                           moving_only=False,
+                                           eval_full_res=True)
+
+
+
+    # def visualize_segments(self, visuals, target, image, prefix=''):
+    #
+    #     matched_cc_preds, matched_gts, cc_ious = visuals['pred_segment']
+    #
+    #     H = W = 128
+    #
+    #     fsz = 19
+    #     num_plots = 2+len(matched_cc_preds[0])*2
+    #     fig = plt.figure(figsize=(num_plots * 4, 5))
+    #     gs = fig.add_gridspec(1, num_plots)
+    #     ax1 = fig.add_subplot(gs[0])
+    #
+    #     plt.imshow(image[0].permute([1, 2, 0]).cpu())
+    #     plt.axis('off')
+    #     ax1.set_title('Image', fontsize=fsz)
+    #
+    #
+    #     # labels = F.interpolate(batched_inputs[0]['gt_moving'].unsqueeze(0).float().cuda(), size=[H, W], mode='nearest')
+    #     ax = fig.add_subplot(gs[1])
+    #
+    #     if target is None:
+    #         target = torch.zeros(1, 1, H, W)
+    #     plt.imshow(target[0].cpu())
+    #     plt.title('Supervision', fontsize=fsz)
+    #     plt.axis('off')
+    #
+    #     for i, (cc_pred, gt, cc_iou) in enumerate(zip(matched_cc_preds[0], matched_gts[0], cc_ious[0])):
+    #         ax = fig.add_subplot(gs[2 + i])
+    #         ax.imshow(cc_pred)
+    #         ax.set_title('Pred (IoU: %.2f)' % cc_iou, fontsize=fsz)
+    #         plt.axis('off')
+    #
+    #         ax = fig.add_subplot(gs[2 + len(matched_cc_preds[0]) + i])
+    #         ax.imshow(gt)
+    #         plt.axis('off')
+    #         ax.set_title('GT %d' % i, fontsize=fsz)
+    #
+    #     # file_idx = batched_inputs[0]['file_name'].split('/')[-1].split('.hdf5')[0]
+    #     # save_path = os.path.join(self.vis_saved_path, 'step_%smask_%s_%s.png' % (prefix, 'eval' if iter is None else str(iter), file_idx))
+    #     # print('Save fig to ', save_path)
+    #     # plt.savefig(save_path, bbox_inches='tight')
+    #
+    #     plt.show()
+    #     plt.close()
+
+    def visualize_segments_all(self, pred_segment, gt_segment, target, image, prefix=''):
+
+        H = W = 64
+
+        fsz = 19
+        num_plots = 4
+        fig = plt.figure(figsize=(num_plots * 4, 5))
+        gs = fig.add_gridspec(1, num_plots)
+        ax1 = fig.add_subplot(gs[0])
+
+        plt.imshow(image[0].permute([1, 2, 0]).cpu())
+        plt.axis('off')
+        ax1.set_title('Image', fontsize=fsz)
+
+
+        # labels = F.interpolate(batched_inputs[0]['gt_moving'].unsqueeze(0).float().cuda(), size=[H, W], mode='nearest')
+        ax = fig.add_subplot(gs[1])
+
+        if target is None:
+            target = torch.zeros(1, 1, H, W)
+        plt.imshow(target[0].cpu())
+        plt.title('Supervision', fontsize=fsz)
+        plt.axis('off')
+
+        ax = fig.add_subplot(gs[2])
+        plt.imshow(pred_segment[0].cpu(), cmap='twilight')
+        plt.title('Pred segments', fontsize=fsz)
+        plt.axis('off')
+
+        ax = fig.add_subplot(gs[3])
+        plt.imshow(gt_segment[0, 0].cpu(), cmap='twilight')
+        plt.title('GT segments', fontsize=fsz)
+        plt.axis('off')
+
+
+        # file_idx = batched_inputs[0]['file_name'].split('/')[-1].split('.hdf5')[0]
+        # save_path = os.path.join(self.vis_saved_path, 'step_%smask_%s_%s.png' % (prefix, 'eval' if iter is None else str(iter), file_idx))
+        # print('Save fig to ', save_path)
+        # plt.savefig(save_path, bbox_inches='tight')
+
+        plt.show()
+        plt.close()
+
+
+    @staticmethod
+    def visualize_targets(img, seg):
+        plt.subplot(1, 2, 1)
+        plt.imshow(img[0].permute(1, 2, 0).cpu() / 255.)
+        plt.axis('off')
+        plt.subplot(1, 2, 2)
+        plt.imshow(seg[0].cpu(), cmap='twilight')
+        plt.axis('off')
+        plt.show()
+        plt.close()
+
+
+
+if __name__ == '__main__':
+    args = get_args()
+
+    motion_params = {
+        'small': False,
+
+    }
+    boundary_params = {
+        'small': False,
+        'static_input': False,
+        'orientation_type': 'regression'
+    }
+
+    teacher_class = 'motion_to_static'
+    teacher_params = {
+        'downsample_factor': 4,
+        'motion_path': args.motion_path,
+        'motion_model_params': motion_params,
+        'boundary_path': args.boundary_path,
+        'boundary_model_params': boundary_params
+    }
+
+    student_class = 'eisen'
+    student_params = {}
+
+
+    teacher_student = TeacherStudent(
+        teacher_class=teacher_class,
+        teacher_params=teacher_params,
+        teacher_load_path=None,
+        student_class=student_class,
+        student_params=student_params,
+        student_load_path=None,
+    )
+
