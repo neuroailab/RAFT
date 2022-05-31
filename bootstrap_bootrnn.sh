dd8e4e0a479d7750799a37226a641a6760173af7
Dan Bear <dbear@node6-ccn2cluster.stanford.edu>
2022-05-30 20:37:38 -0700

    back to bs=2

--- bootstrap_bootrnn.sh
+++ bootstrap_bootrnn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p checkpoints
 # 5.11.22
-python -u train_bootrnn.py --name topobootrnn-movi_d-bs4-0 --stage movi_d --validation z --gpus 0 --num_steps 100000 --batch_size 4 --lr 0.0004 --image_size 496 496 --wdecay 0.0001 --model bootrnn --no_aug --max_frame 0 --val_freq 2500 --save_students
+python -u train_bootrnn.py --name topobootrnn-movi_d-bs2-0 --stage movi_d --validation z --gpus 0 --num_steps 100000 --batch_size 2 --lr 0.0004 --image_size 496 496 --wdecay 0.0001 --model bootrnn --no_aug --max_frame 0 --val_freq 2500 --save_students
